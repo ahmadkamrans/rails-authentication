373c527e34195d80d7295a6e5729c3119f54af25
Ahmad Kamran <ahmad.eritheia@gmail.com>
2022-08-27 16:06:52 +0500

    Pundit basic authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::Base
+	protect_from_forgery prepend: true
+	before_action :authenticate_user!
+
+	rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 end
